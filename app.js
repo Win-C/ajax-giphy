a30ab381e4f44d9ef0669ab442acb08044b21c9d
Winnie Chou <winnie.chou.mb@gmail.com>
2020-11-17 18:41:40 -0600

    part 1 done

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+"use strict";
+
+const url = 'http://api.giphy.com/v1/gifs/search';
+const api_key = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
+
+$(function(){
+
+function appendToDom(imageUrl){
+  $(".gifs").append(`<img src=${imageUrl}>`);
+}
+
+async function getSearch(){
+  // debugger;
+  let searchWord = $("input").val();
+  let response = await axios.get(url, {params: {q: searchWord, api_key}});
+  console.log("getSearch resp=", response);
+  appendToDom(response.data.data[0].images.original.url);
+};
+
+$("#submit").on("click", getSearch);
+
 console.log("Let's get this party started!");
+
+});
